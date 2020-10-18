0f5ff1599115cd754d6293af7452fb9ab4571bc2
Marko <hawk691@gmail.com>
2020-10-18 17:35:37 +0300

    improoved classes for choice fields

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,29 +3,30 @@ from .models import *
 from django import forms
 
 
-class NotebookCategoryChoiceField(forms.ModelChoiceField):
+class CategoryChoiceField(forms.ModelChoiceField):
     pass
 
-
-class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
-    pass
 
 class NotebookAdmin(admin.ModelAdmin):
+    """
+    Class representing choice field for form in notebook adding
+    """
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug='notebooks'))
+            return CategoryChoiceField(Category.objects.filter(slug='notebooks'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
+    """
+    Class representing choice field for form in smartphones adding
+    """
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return SmartphoneCategoryChoiceField(Category.objects.filter(slug='smartphones'))
+            return CategoryChoiceField(Category.objects.filter(slug='smartphones'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
 
 
 admin.site.register(Category)
